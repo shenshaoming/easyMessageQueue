2bde566788c869929a349de578e30c23330d47b7
shenshaoming <626546063@qq.com>
2020-04-06 16:18:32 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+# easyMessageQueue
+A easy MessageQueue project, just for study.
+<br>一个简单实现了消息队列的项目,仅供学习.
--- src/main/com/rrtx/shen/core/Main.java
+++ src/main/com/rrtx/shen/core/Main.java
@@ -5,4 +5,7 @@ package main.com.rrtx.shen.core;
  * @date 2020/4/6 15:58
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("启动类");
+    }
 }
